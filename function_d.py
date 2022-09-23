05e0f20f4f7b3c2eb5d29c6c35278bdd60883b84
Billie Betts <lily.a.betts@gmail.com>
2022-09-23 13:41:52 -0400

    My version of f_d

--- function_d.py
+++ function_d.py
@@ -2,7 +2,16 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+
+    max_testing = numbers[0]
+
+    for n in range(numbers):
+        if numbers[n] > max_testing:
+            max_testing = numbers[n]
+
+    return max_testing
+
+    
 
 
 if __name__ == "__main__":
