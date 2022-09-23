5af162b4ca50447a6c1cce737a1757a0d9abf94b
Billie Betts <lily.a.betts@gmail.com>
2022-09-23 13:34:54 -0400

    Wrote function a

--- function_a.py
+++ function_a.py
@@ -1,7 +1,25 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    most_occurences_count = {}
+
+    for num in range(number_list):
+        most_occurences_count[num] = 0
+
+    for each_num in most_occurences_count():
+        real_count = most_occurences_count.count(each_num)
+        most_occurences_count[each_num] = real_count
+
+    most_common_num = None
+    num_occured = 0
+
+    for n in most_occurences_count.keys():
+        if most_occurences_count[n] > num_occured:
+            most_common_num = n
+            num_occured = most_occurences_count[n]
+    
+    return most_common_num
+
 
 
 if __name__ == "__main__":
